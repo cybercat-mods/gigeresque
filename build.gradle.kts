2922e42c3cba139f2499ffbfc516e04ab248cb89
cybercat5555 <cybercat5555@gmail.com>
2022-02-05 12:43:42 -0500

    Start of import

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("fabric-loom")
+    val kotlinVersion: String by System.getProperties()
+    kotlin("jvm").version(kotlinVersion)
+}
+base {
+    val archivesBaseName: String by project
+    archivesName.set(archivesBaseName)
+}
+val modVersion: String by project
+version = modVersion
+val mavenGroup: String by project
+group = mavenGroup
+minecraft {}
+repositories {
+    mavenCentral()
+    maven(url = "https://dl.cloudsmith.io/public/geckolib3/geckolib/maven/")
+    maven(url = "https://maven.terraformersmc.com/releases/")
+    maven(url = "https://maven.shedaniel.me/")
+}
+dependencies {
+    val minecraftVersion: String by project
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    val yarnMappings: String by project
+    mappings("net.fabricmc:yarn:$yarnMappings:v2")
+    val loaderVersion: String by project
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    val fabricVersion: String by project
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+    val fabricKotlinVersion: String by project
+    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
+    val geckoLibVersion: String by project
+    modImplementation("software.bernie.geckolib:geckolib-fabric-1.17:$geckoLibVersion")
+    val modMenuVersion: String by project
+    modImplementation("com.terraformersmc:modmenu:$modMenuVersion")
+
+    modApi("me.shedaniel.cloth:cloth-config-fabric:5.0.38") {
+        exclude("net.fabricmc.fabric-api")
+    }
+}
+tasks {
+    val javaVersion = JavaVersion.VERSION_16
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions { jvmTarget = javaVersion.toString() }
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+    }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
+    }
+}
