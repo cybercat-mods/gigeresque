9ec390bc4b31fcb69958542d37da77480cd34b91
AzureZhen <rcraft@cyberconnects2.com>
2022-03-03 03:56:21 -0500

    Structures generate again and are datapackable.

--- src/main/java/mods/cybercat/gigeresque/common/structures/Structures.java
+++ src/main/java/mods/cybercat/gigeresque/common/structures/Structures.java
@@ -1,23 +1,17 @@
 package mods.cybercat.gigeresque.common.structures;
 
 import mods.cybercat.gigeresque.common.Gigeresque;
-
-import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
-import net.minecraft.util.Identifier;
+import mods.cybercat.gigeresque.mixins.common.StructureFeatureAccessor;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
-import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 
 public class Structures {
 
-	public static StructureFeature<StructurePoolFeatureConfig> GIG_DUNGEON = new GigDungeonStructure(
-			StructurePoolFeatureConfig.CODEC);
+	public static StructureFeature<?> GIG_DUNGEON = new GigDungeonStructure();
 
 	public static void setupAndRegisterStructureFeatures() {
-		FabricStructureBuilder.create(new Identifier(Gigeresque.MOD_ID, "gig_dungeon"), GIG_DUNGEON)
-				.step(GenerationStep.Feature.SURFACE_STRUCTURES).defaultConfig(new StructureConfig(80, 20, 1234567890))
-				.adjustsSurface().register();
+		StructureFeatureAccessor.callRegister(Gigeresque.MOD_ID + ":gig_dungeon", GIG_DUNGEON,
+				GenerationStep.Feature.SURFACE_STRUCTURES);
 	}
 
 }
