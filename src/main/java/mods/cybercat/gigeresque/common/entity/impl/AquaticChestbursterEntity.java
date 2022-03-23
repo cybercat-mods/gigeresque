d7ab6c7ec5a510f901332daddc6c997b98b81f03
AzureZhen <rcraft@cyberconnects2.com>
2022-03-23 02:02:33 -0400

    Swim my little chestbuster!

--- src/main/java/mods/cybercat/gigeresque/common/entity/impl/AquaticChestbursterEntity.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/impl/AquaticChestbursterEntity.java
@@ -1,13 +1,88 @@
 package mods.cybercat.gigeresque.common.entity.impl;
 
 import mods.cybercat.gigeresque.common.entity.Growable;
-
+import mods.cybercat.gigeresque.common.entity.ai.pathing.AmphibiousNavigation;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.ai.control.AquaticMoveControl;
+import net.minecraft.entity.ai.control.LookControl;
+import net.minecraft.entity.ai.control.MoveControl;
+import net.minecraft.entity.ai.control.YawAdjustingLookControl;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.MobNavigation;
+import net.minecraft.entity.ai.pathing.PathNodeType;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 
 public class AquaticChestbursterEntity extends ChestbursterEntity implements IAnimatable, Growable {
+
+	private final MobNavigation landNavigation = new MobNavigation(this, world);
+	private final AmphibiousNavigation swimNavigation = new AmphibiousNavigation(this, world);
+
+	private final MoveControl landMoveControl = new MoveControl(this);
+	private final LookControl landLookControl = new LookControl(this);
+	private final AquaticMoveControl swimMoveControl = new AquaticMoveControl(this, 85, 10, 0.7f, 1.0f, false);
+	private final YawAdjustingLookControl swimLookControl = new YawAdjustingLookControl(this, 10);
+	
 	public AquaticChestbursterEntity(EntityType<? extends AquaticChestbursterEntity> type, World world) {
 		super(type, world);
+		ignoreCameraFrustum = true;
+		stepHeight = 1.0f;
+
+		navigation = swimNavigation;
+		moveControl = swimMoveControl;
+		lookControl = swimLookControl;
+		setPathfindingPenalty(PathNodeType.WATER, 0.0f);
+	}
+
+	@Override
+	public void travel(Vec3d movementInput) {
+		this.navigation = (this.isSubmergedInWater() || this.isTouchingWater()) ? swimNavigation : landNavigation;
+		this.moveControl = (this.submergedInWater || this.isTouchingWater()) ? swimMoveControl : landMoveControl;
+		this.lookControl = (this.submergedInWater || this.isTouchingWater()) ? swimLookControl : landLookControl;
+
+		if (this.age % 10 == 0) {
+			this.calculateDimensions();
+		}
+
+		if (canMoveVoluntarily() && this.isTouchingWater()) {
+			updateVelocity(getMovementSpeed(), movementInput);
+			move(MovementType.SELF, getVelocity());
+			setVelocity(getVelocity().multiply(0.9));
+			if (getTarget() == null) {
+				setVelocity(getVelocity().add(0.0, -0.005, 0.0));
+			}
+		} else {
+			super.travel(movementInput);
+		}
+	}
+
+	@Override
+	public boolean canBreatheInWater() {
+		return true;
+	}
+
+	@Override
+	public EntityNavigation createNavigation(World world) {
+		return swimNavigation;
+	}
+
+	@Override
+	public boolean isPushedByFluids() {
+		return false;
+	}
+
+	@Override
+	protected void swimUpward(TagKey<Fluid> fluid) {
+	}
+
+	@Override
+	public EntityDimensions getDimensions(EntityPose pose) {
+		return this.submergedInWater ? super.getDimensions(pose).scaled(1.0f, 0.5f) : super.getDimensions(pose);
 	}
 }
