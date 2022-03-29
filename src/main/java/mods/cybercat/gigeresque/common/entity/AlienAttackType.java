c546dd0f3c2fcda05d6ca7378a6d70a8c9c330ca
AzureZhen <rcraft@cyberconnects2.com>
2022-03-29 01:23:57 -0400

    Removes tail over and new animation names

--- src/main/java/mods/cybercat/gigeresque/common/entity/AlienAttackType.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/AlienAttackType.java
@@ -5,11 +5,10 @@ import java.util.Map;
 public enum AlienAttackType {
 	NONE(GenericAlienAttackType.NONE), CLAW_LEFT(GenericAlienAttackType.CLAW), CLAW_RIGHT(GenericAlienAttackType.CLAW),
 	HEAD_BITE(GenericAlienAttackType.BITE), TAIL_LEFT(GenericAlienAttackType.TAIL),
-	TAIL_OVER(GenericAlienAttackType.TAIL), TAIL_RIGHT(GenericAlienAttackType.TAIL);
+	TAIL_RIGHT(GenericAlienAttackType.TAIL);
 
-	public static final Map<AlienAttackType, String> animationMappings = Map.of(CLAW_LEFT, "attacking_leftclaw",
-			CLAW_RIGHT, "attacking_rightclaw", HEAD_BITE, "attacking_innerjaw", TAIL_LEFT, "attacking_tail_left",
-			TAIL_OVER, "attacking_tail_over", TAIL_RIGHT, "attacking_tail_right");
+	public static final Map<AlienAttackType, String> animationMappings = Map.of(CLAW_LEFT, "left_claw", CLAW_RIGHT,
+			"right_claw", HEAD_BITE, "head_bite", TAIL_LEFT, "left_tail", TAIL_RIGHT, "right_tail");
 
 	public final GenericAlienAttackType genericAttackType;
 
