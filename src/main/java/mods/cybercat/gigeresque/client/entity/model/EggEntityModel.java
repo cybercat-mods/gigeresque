fe7962c7d8a42aa4a31850754e17745eb26ccfef
AzureZhen <rcraft@cyberconnects2.com>
2022-04-01 01:36:36 -0400

    New Egg Functions

--- src/main/java/mods/cybercat/gigeresque/client/entity/model/EggEntityModel.java
+++ src/main/java/mods/cybercat/gigeresque/client/entity/model/EggEntityModel.java
@@ -2,8 +2,8 @@ package mods.cybercat.gigeresque.client.entity.model;
 
 import mods.cybercat.gigeresque.client.entity.animation.EntityAnimations;
 import mods.cybercat.gigeresque.client.entity.texture.EntityTextures;
+import mods.cybercat.gigeresque.common.Gigeresque;
 import mods.cybercat.gigeresque.common.entity.impl.AlienEggEntity;
-
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.util.Identifier;
@@ -18,7 +18,7 @@ public class EggEntityModel extends AnimatedGeoModel<AlienEggEntity> {
 
 	@Override
 	public Identifier getTextureLocation(AlienEggEntity object) {
-		return EntityTextures.EGG;
+		return object.isHatching() || object.isHatched() ? new Identifier(Gigeresque.MOD_ID, "textures/entity/egg/egg_active.png"): EntityTextures.EGG;
 	}
 
 	@Override
