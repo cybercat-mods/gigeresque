b792d95af53df44d3bb42767087b97674b357f6c
AzureZhen <rcraft@cyberconnects2.com>
2022-04-13 02:43:16 -0400

    Update FlightMoveController.java

--- src/main/java/mods/cybercat/gigeresque/common/entity/ai/pathing/FlightMoveController.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/ai/pathing/FlightMoveController.java
@@ -2,29 +2,13 @@ package mods.cybercat.gigeresque.common.entity.ai.pathing;
 
 import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 
 public class FlightMoveController extends MoveControl {
 	private final MobEntity parentEntity;
-	private final float speedGeneral;
-	private final boolean shouldLookAtTarget;
-	private final boolean needsYSupport;
 
-	public FlightMoveController(MobEntity bird, float speedGeneral, boolean shouldLookAtTarget, boolean needsYSupport) {
+	public FlightMoveController(MobEntity bird) {
 		super(bird);
 		this.parentEntity = bird;
-		this.shouldLookAtTarget = shouldLookAtTarget;
-		this.speedGeneral = speedGeneral;
-		this.needsYSupport = needsYSupport;
-	}
-
-	public FlightMoveController(MobEntity bird, float speedGeneral, boolean shouldLookAtTarget) {
-		this(bird, speedGeneral, shouldLookAtTarget, false);
-	}
-
-	public FlightMoveController(MobEntity bird, float speedGeneral) {
-		this(bird, speedGeneral, true);
 	}
 
 	public void tick() {
