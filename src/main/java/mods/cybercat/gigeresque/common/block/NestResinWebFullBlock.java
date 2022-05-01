ed7b6e29b246293a1e2b31ee9ea3a9428e010dd8
AzureZhen <rcraft@cyberconnects2.com>
2022-04-30 20:19:43 -0400

    Closes #36

--- src/main/java/mods/cybercat/gigeresque/common/block/NestResinWebFullBlock.java
+++ src/main/java/mods/cybercat/gigeresque/common/block/NestResinWebFullBlock.java
@@ -1,10 +1,12 @@
 package mods.cybercat.gigeresque.common.block;
 
 import mods.cybercat.gigeresque.common.entity.AlienEntity;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -16,8 +18,9 @@ public class NestResinWebFullBlock extends Block {
 
 	@Override
 	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-		if (!(entity instanceof AlienEntity)) {
+		if (!(entity instanceof AlienEntity) && entity instanceof LivingEntity) {
 			entity.slowMovement(state, new Vec3d(0.25, 0.05000000074505806, 0.25));
+			((LivingEntity)entity).setStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 60, 10), entity);
 		}
 	}
 }
