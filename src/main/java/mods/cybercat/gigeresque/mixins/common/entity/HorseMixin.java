6b7b9a0f6989c7af98017484981c557e4bff0da0
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-03-05 12:19:16 -0500

    Closes #137

---
+++ src/main/java/mods/cybercat/gigeresque/mixins/common/entity/HorseMixin.java
@@ -0,0 +1,30 @@
+package mods.cybercat.gigeresque.mixins.common.entity;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import mods.cybercat.gigeresque.common.entity.AlienEntity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.level.Level;
+
+@Mixin(AbstractHorse.class)
+public abstract class HorseMixin extends Animal {
+
+	public HorseMixin(EntityType<? extends Animal> entityType, Level level) {
+		super(entityType, level);
+	}
+
+	@Inject(method = { "tick" }, at = { @At("HEAD") }, cancellable = true)
+	void noBuckling(CallbackInfo callbackInfo) {
+		if (this.getPassengers().stream().anyMatch(AlienEntity.class::isInstance)) {
+			this.removeFreeWill();
+			this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 400, 10));
+		}
+	}
+}
