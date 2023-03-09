7957a96be733d5e78442b35ea637a21dbb6a54f0
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-03-09 16:07:23 -0500

    Lock the stasis is the direction the xeno is looking when going into stasis.

--- src/main/java/mods/cybercat/gigeresque/common/entity/impl/AdultAlienEntity.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/impl/AdultAlienEntity.java
@@ -289,8 +289,8 @@ public abstract class AdultAlienEntity extends AlienEntity implements GeoEntity,
 				setIsStatis(true);
 				this.xxa = 0.0f;
 				this.zza = 0.0f;
-				this.yHeadRot = 0.0f;
-				this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 10, 100, false, true));
+				this.yHeadRot = this.yRot;
+				this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 10, 100, false, false));
 			}
 		} else {
 			setStatisTimer(0);
