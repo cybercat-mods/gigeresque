3126159aedddfb6594ea4fca7d84eb2cbbdcb45c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-06-19 00:03:35 -0400

    Adjust Stalkers walking trigger for invis

--- src/main/java/mods/cybercat/gigeresque/client/entity/render/StalkerEntityRenderer.java
+++ src/main/java/mods/cybercat/gigeresque/client/entity/render/StalkerEntityRenderer.java
@@ -28,7 +28,7 @@ public class StalkerEntityRenderer extends GeoEntityRenderer<StalkerEntity> {
 
 	@Override
 	public void actuallyRender(PoseStack poseStack, StalkerEntity animatable, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender,  partialTick, packedLight, packedOverlay, red, green, blue, animatable.walkAnimation.speedOld < 0.35F && !animatable.swinging ? 0.11F : 1.0F);
+		super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender,  partialTick, packedLight, packedOverlay, red, green, blue, animatable.walkAnimation.speedOld < 0.45F && !animatable.swinging ? 0.11F : 1.0F);
 	}
 
 	@Override
