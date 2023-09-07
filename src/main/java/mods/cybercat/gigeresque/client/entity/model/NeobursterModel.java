ba16c0192dce4135ac01637936df73758d85728b
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-07 18:30:21 -0400

    Tweak

--- src/main/java/mods/cybercat/gigeresque/client/entity/model/NeobursterModel.java
+++ src/main/java/mods/cybercat/gigeresque/client/entity/model/NeobursterModel.java
@@ -1,5 +1,8 @@
 package mods.cybercat.gigeresque.client.entity.model;
 
+import mod.azure.azurelib.constant.DataTickets;
+import mod.azure.azurelib.core.animatable.model.CoreGeoBone;
+import mod.azure.azurelib.core.animation.AnimationState;
 import mod.azure.azurelib.model.DefaultedEntityGeoModel;
 import mods.cybercat.gigeresque.Constants;
 import mods.cybercat.gigeresque.common.entity.impl.neo.NeobursterEntity;
@@ -7,6 +10,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 @Environment(EnvType.CLIENT)
 public class NeobursterModel extends DefaultedEntityGeoModel<NeobursterEntity> {
@@ -18,6 +22,18 @@ public class NeobursterModel extends DefaultedEntityGeoModel<NeobursterEntity> {
 	@Override
 	public RenderType getRenderType(NeobursterEntity animatable, ResourceLocation texture) {
 		return RenderType.entityTranslucent(getTextureResource(animatable));
+	}
+	
+	@Override
+	public void setCustomAnimations(NeobursterEntity animatable, long instanceId, AnimationState<NeobursterEntity> animationState) {
+
+		CoreGeoBone head = getAnimationProcessor().getBone("main");
+
+		if (head != null) 
+			head.setRotY(animationState.getData(DataTickets.ENTITY_MODEL_DATA).headPitch() * Mth.DEG_TO_RAD); 
+		
+		
+		super.setCustomAnimations(animatable, instanceId, animationState);
 	}
 
 }
