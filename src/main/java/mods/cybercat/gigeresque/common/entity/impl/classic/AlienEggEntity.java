f006c9ae06ab52b44c081cf55d9b105f61308a3f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-09 11:12:28 -0400

    Fixes Eggs opening when spawned due to broken check.

--- src/main/java/mods/cybercat/gigeresque/common/entity/impl/classic/AlienEggEntity.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/impl/classic/AlienEggEntity.java
@@ -265,7 +265,7 @@ public class AlienEggEntity extends AlienEntity implements GeoEntity {
 		var aabb = new AABB(this.blockPosition().above()).inflate(Gigeresque.config.alieneggHatchRange);
 		this.level().getEntitiesOfClass(LivingEntity.class, aabb).forEach(target -> {
 			if (target.isAlive())
-				if (GigEntityUtils.faceHuggerTest(target, this))
+				if (GigEntityUtils.faceHuggerTest(target, this) && !(target instanceof Player))
 					setIsHatching(true);
 				else if (target instanceof Player player && !(player.isCreative() || !player.isSpectator()))
 					setIsHatching(true);
