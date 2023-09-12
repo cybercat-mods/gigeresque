e976a504e0b79a74fdd7f25d56e5de9b62ed0e78
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-12 05:13:11 -0400

    Built-in compatibility packs added, major thanks to impulsivelass

--- src/main/java/mods/cybercat/gigeresque/common/Gigeresque.java
+++ src/main/java/mods/cybercat/gigeresque/common/Gigeresque.java
@@ -3,6 +3,7 @@ package mods.cybercat.gigeresque.common;
 import mod.azure.azurelib.AzureLibMod;
 import mod.azure.azurelib.config.format.ConfigFormats;
 import mods.cybercat.gigeresque.common.block.GIgBlocks;
+import mods.cybercat.gigeresque.common.compat.GigCompats;
 import mods.cybercat.gigeresque.common.config.GigeresqueConfig;
 import mods.cybercat.gigeresque.common.data.handler.TrackedDataHandlers;
 import mods.cybercat.gigeresque.common.entity.Entities;
@@ -41,6 +42,7 @@ public class Gigeresque implements ModInitializer {
 		GigStructures.registerStructureFeatures();
 		ServerLifecycleEvents.SERVER_STARTED.register(minecraftServer -> GigVillagerTrades.addTrades());
 		GigItemGroups.getInstance().initialize();
+		GigCompats.getInstance().initialize();;
 		FlammableBlockRegistry.getDefaultInstance().add(GIgBlocks.NEST_RESIN_BLOCK, 20, 5);
 		FlammableBlockRegistry.getDefaultInstance().add(GIgBlocks.NEST_RESIN, 20, 5);
 		FlammableBlockRegistry.getDefaultInstance().add(GIgBlocks.NEST_RESIN_WEB, 20, 5);
