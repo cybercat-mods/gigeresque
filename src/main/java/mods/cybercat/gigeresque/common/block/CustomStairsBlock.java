b903a14ff3b5daae66221de5b741e485c6dc1724
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-12 20:16:26 -0400

    Update CustomStairsBlock.java

--- src/main/java/mods/cybercat/gigeresque/common/block/CustomStairsBlock.java
+++ src/main/java/mods/cybercat/gigeresque/common/block/CustomStairsBlock.java
@@ -1,8 +1,11 @@
 package mods.cybercat.gigeresque.common.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 
 public class CustomStairsBlock extends StairBlock {
 	public CustomStairsBlock(BlockState baseBlockState, Properties settings) {
@@ -15,5 +18,10 @@ public class CustomStairsBlock extends StairBlock {
 
 	public CustomStairsBlock(Block block) {
 		this(block, Properties.copy(block));
+	}
+
+	@Override
+	public boolean isPathfindable(BlockState state, BlockGetter world, BlockPos pos, PathComputationType type) {
+		return false;
 	}
 }
