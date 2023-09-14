0c799066041946e5b197fba5b815e034f731a967
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-14 03:02:29 -0400

    Fix up Find Darkness Task

--- src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/FindDarknessTask.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/FindDarknessTask.java
@@ -1,26 +1,35 @@
 package mods.cybercat.gigeresque.common.entity.ai.tasks;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.jetbrains.annotations.Nullable;
 
 import com.mojang.datafixers.util.Pair;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import mods.cybercat.gigeresque.common.block.GigBlocks;
+import mods.cybercat.gigeresque.common.entity.impl.AdultAlienEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.memory.WalkTarget;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
 import net.tslat.smartbrainlib.util.BrainUtils;
 
-public class FindDarknessTask<E extends PathfinderMob> extends ExtendedBehaviour<E> {
+public class FindDarknessTask<E extends AdultAlienEntity> extends ExtendedBehaviour<E> {
 	private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(Pair.of(MemoryModuleType.ATTACK_TARGET, MemoryStatus.VALUE_ABSENT), Pair.of(MemoryModuleType.WALK_TARGET, MemoryStatus.REGISTERED));
 
 	protected float speedModifier = 1;
+
+	protected Function<E, Integer> attackIntervalSupplier = entity -> 80;
+
+	public static final Predicate<BlockState> NEST = state -> state.is(GigBlocks.NEST_RESIN_WEB_CROSS);
 
 	protected Vec3 hidePos = null;
 
@@ -37,12 +46,10 @@ public class FindDarknessTask<E extends PathfinderMob> extends ExtendedBehaviour
 
 	@Override
 	protected boolean checkExtraStartConditions(ServerLevel level, E entity) {
-		if (!level.canSeeSky(entity.blockPosition()))
-			return false;
 
 		this.hidePos = getHidePos(entity);
 
-		return !entity.isVehicle() && !entity.isAggressive() && this.hidePos != null;
+		return !entity.level().getBlockStatesIfLoaded(entity.getBoundingBox().inflate(32)).anyMatch(NEST) && !entity.isVehicle() && !entity.isAggressive() && !entity.isPassedOut() && this.hidePos != null && entity.level().getBrightness(LightLayer.SKY, entity.blockPosition()) > 5;
 	}
 
 	@Override
@@ -65,7 +72,7 @@ public class FindDarknessTask<E extends PathfinderMob> extends ExtendedBehaviour
 
 	@Override
 	protected boolean timedOut(long gameTime) {
-		return false;
+		return super.timedOut(gameTime);
 	}
 
 	@Override
@@ -78,7 +85,7 @@ public class FindDarknessTask<E extends PathfinderMob> extends ExtendedBehaviour
 		var randomsource = entity.getRandom();
 		var entityPos = entity.blockPosition();
 
-		for (int i = 0; i < 50; ++i) {
+		for (var i = 0; i < 50; ++i) {
 			var hidePos = entityPos.offset(randomsource.nextInt(20) - 50, randomsource.nextInt(6) - 3, randomsource.nextInt(20) - 50);
 
 			if (!entity.level().canSeeSky(hidePos) && entity.getWalkTargetValue(hidePos) < 0.0F)
