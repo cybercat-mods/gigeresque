aca377bc746f4ef5c17206d44b8788e25b7987b3
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-19 14:44:39 -0400

    Update BlackFluid.java

--- src/main/java/mods/cybercat/gigeresque/common/fluid/BlackFluid.java
+++ src/main/java/mods/cybercat/gigeresque/common/fluid/BlackFluid.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
