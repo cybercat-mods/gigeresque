2597e5f0b8d48767d19bccd62519e3a7c08ee3c3
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-10-02 17:56:13 -0400

    Update AlienEntityModel.java

--- src/main/java/mods/cybercat/gigeresque/client/entity/model/AlienEntityModel.java
+++ src/main/java/mods/cybercat/gigeresque/client/entity/model/AlienEntityModel.java
@@ -1,7 +1,5 @@
 package mods.cybercat.gigeresque.client.entity.model;
 
-import mod.azure.azurelib.constant.DataTickets;
-import mod.azure.azurelib.core.animatable.model.CoreGeoBone;
 import mod.azure.azurelib.core.animation.AnimationState;
 import mod.azure.azurelib.model.DefaultedEntityGeoModel;
 import mods.cybercat.gigeresque.Constants;
@@ -11,7 +9,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 
 @Environment(EnvType.CLIENT)
 public class AlienEntityModel extends DefaultedEntityGeoModel<ClassicAlienEntity> {
@@ -32,13 +29,6 @@ public class AlienEntityModel extends DefaultedEntityGeoModel<ClassicAlienEntity
 
 	@Override
 	public void setCustomAnimations(ClassicAlienEntity animatable, long instanceId, AnimationState<ClassicAlienEntity> animationState) {
-
-		CoreGeoBone head = getAnimationProcessor().getBone("head");
-
-		if (head != null && animatable.getDeltaMovement().horizontalDistance() == 0 && !animatable.isVehicle() && !animatable.isPassedOut()) {
-//			head.setRotX(animationState.getData(DataTickets.ENTITY_MODEL_DATA).headPitch() * Mth.DEG_TO_RAD);
-			head.setRotY(animationState.getData(DataTickets.ENTITY_MODEL_DATA).netHeadYaw() * Mth.DEG_TO_RAD);
-		}
 
 		super.setCustomAnimations(animatable, instanceId, animationState);
 	}
