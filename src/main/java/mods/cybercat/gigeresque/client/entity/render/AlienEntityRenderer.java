ffe2729d43cc0bf14ac902c3a3a015cc2b1a7503
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-15 00:18:08 -0400

    Fixes model placement

--- src/main/java/mods/cybercat/gigeresque/client/entity/render/AlienEntityRenderer.java
+++ src/main/java/mods/cybercat/gigeresque/client/entity/render/AlienEntityRenderer.java
@@ -1,30 +1,15 @@
 package mods.cybercat.gigeresque.client.entity.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import mod.azure.azurelib.cache.object.BakedGeoModel;
 import mod.azure.azurelib.renderer.GeoEntityRenderer;
-import mod.azure.bettercrawling.entity.mob.Orientation;
-import mod.azure.bettercrawling.entity.mob.PathingTarget;
 import mods.cybercat.gigeresque.client.entity.model.AlienEntityModel;
 import mods.cybercat.gigeresque.client.entity.render.feature.ClassicAlienFeatureRenderer;
 import mods.cybercat.gigeresque.common.entity.impl.classic.ClassicAlienEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Matrix4f;
-
-import java.util.Iterator;
-import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class AlienEntityRenderer extends GeoEntityRenderer<ClassicAlienEntity> {
@@ -39,9 +24,8 @@ public class AlienEntityRenderer extends GeoEntityRenderer<ClassicAlienEntity> {
     public void render(ClassicAlienEntity entity, float entityYaw, float partialTick, PoseStack stack, @NotNull MultiBufferSource bufferSource, int packedLightIn) {
         var scaleFactor = 0.8f + ((entity.getGrowth() / entity.getMaxGrowth()) / 5f);
         stack.scale(scaleFactor, scaleFactor, scaleFactor);
-        if (entity.isCrawling() && entity.level() != null && entity.level().getBlockState(
-                entity.blockPosition().above()).isSolid()) {
-            stack.translate(0, -0.5, 0);
+        if (entity.onGround()) {
+            stack.translate(0, -0.2, 0);
         }
         super.render(entity, entityYaw, partialTick, stack, bufferSource, packedLightIn);
     }
