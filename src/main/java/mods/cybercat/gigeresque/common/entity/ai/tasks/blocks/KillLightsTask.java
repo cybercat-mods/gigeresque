908bc4eb931589bb29b4d979660b15335a420f19
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-15 18:34:41 -0400

    Update KillLightsTask.java

--- src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/blocks/KillLightsTask.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/blocks/KillLightsTask.java
@@ -12,12 +12,14 @@ import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.level.GameRules;
 import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class KillLightsTask<E extends AlienEntity> extends ExtendedBehaviour<E> {
 
-    private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(Pair.of(GigMemoryTypes.NEARBY_LIGHT_BLOCKS.get(), MemoryStatus.VALUE_PRESENT));
+    private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(
+            Pair.of(GigMemoryTypes.NEARBY_LIGHT_BLOCKS.get(), MemoryStatus.VALUE_PRESENT));
 
     @Override
     protected List<Pair<MemoryModuleType<?>, MemoryStatus>> getMemoryRequirements() {
@@ -30,31 +32,37 @@ public class KillLightsTask<E extends AlienEntity> extends ExtendedBehaviour<E> 
     }
 
     @Override
-    protected boolean canStillUse(ServerLevel level, E entity, long gameTime) {
+    protected boolean canStillUse(@NotNull ServerLevel level, E entity, long gameTime) {
         return !entity.isAggressive();
     }
 
     @Override
-    protected boolean checkExtraStartConditions(ServerLevel level, E entity) {
+    protected boolean checkExtraStartConditions(@NotNull ServerLevel level, E entity) {
         var lightSourceLocation = entity.getBrain().getMemory(GigMemoryTypes.NEARBY_LIGHT_BLOCKS.get()).orElse(null);
+        if (lightSourceLocation == null) return false;
+        if (lightSourceLocation.stream().findFirst().isEmpty()) return false;
         var yDiff = Mth.abs(entity.getBlockY() - lightSourceLocation.stream().findFirst().get().getFirst().getY());
         var canGrief = entity.level().getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING);
         return !entity.isVehicle() && yDiff < 3 && !entity.isAggressive() && canGrief;
     }
 
     @Override
-    protected void tick(ServerLevel level, E entity, long gameTime) {
+    protected void tick(@NotNull ServerLevel level, E entity, long gameTime) {
         var lightSourceLocation = entity.getBrain().getMemory(GigMemoryTypes.NEARBY_LIGHT_BLOCKS.get()).orElse(null);
-        if (lightSourceLocation == null)
-            return;
-        if (!lightSourceLocation.stream().findFirst().get().getFirst().closerToCenterThan(entity.position(), 3.4))
-            startMovingToTarget(entity, lightSourceLocation.stream().findFirst().get().getFirst());
-        if (lightSourceLocation.stream().findFirst().get().getFirst().closerToCenterThan(entity.position(), 3.3)) {
-            entity.triggerAnim("attackController", "swipe");
-            entity.level().destroyBlock(lightSourceLocation.stream().findFirst().get().getFirst(), true, null, 512);
-            if (!entity.level().isClientSide())
-                for (var i = 0; i < 2; i++)
-                    ((ServerLevel) entity.level()).sendParticles(ParticleTypes.POOF, ((double) lightSourceLocation.stream().findFirst().get().getFirst().getX()) + 0.5, lightSourceLocation.stream().findFirst().get().getFirst().getY(), ((double) lightSourceLocation.stream().findFirst().get().getFirst().getZ()) + 0.5, 1, entity.getRandom().nextGaussian() * 0.02, entity.getRandom().nextGaussian() * 0.02, entity.getRandom().nextGaussian() * 0.02, 0.15000000596046448);
+        if (lightSourceLocation != null && lightSourceLocation.stream().findFirst().isPresent()) {
+            var blockPos = lightSourceLocation.stream().findFirst().get().getFirst();
+            if (!blockPos.closerToCenterThan(entity.position(), 3.4)) startMovingToTarget(entity, blockPos);
+            if (blockPos.closerToCenterThan(entity.position(), 3.3)) {
+                entity.triggerAnim("attackController", "swipe");
+                entity.level().destroyBlock(blockPos, true, null, 512);
+                if (!entity.level().isClientSide()) {
+                    for (var i = 0; i < 2; i++)
+                        ((ServerLevel) entity.level()).sendParticles(ParticleTypes.POOF,
+                                ((double) blockPos.getX()) + 0.5, blockPos.getY(), ((double) blockPos.getZ()) + 0.5, 1,
+                                entity.getRandom().nextGaussian() * 0.02, entity.getRandom().nextGaussian() * 0.02,
+                                entity.getRandom().nextGaussian() * 0.02, 0.15000000596046448);
+                }
+            }
         }
     }
 
