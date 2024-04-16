e9751160843a1540ca6217f93575a458714bf0d8
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-16 17:28:14 -0400

    Fixes Eggmorphing a target sometimes just idling

--- src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/movement/EggmorpthTargetTask.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/movement/EggmorpthTargetTask.java
@@ -18,12 +18,14 @@ import net.minecraft.world.phys.Vec3;
 import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
 import net.tslat.smartbrainlib.util.BrainUtils;
 import net.tslat.smartbrainlib.util.RandomUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Objects;
 
 public class EggmorpthTargetTask<E extends AlienEntity> extends ExtendedBehaviour<E> {
-    private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(Pair.of(GigMemoryTypes.NEARBY_NEST_BLOCKS.get(), MemoryStatus.VALUE_PRESENT));
+    private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(
+            Pair.of(GigMemoryTypes.NEARBY_NEST_BLOCKS.get(), MemoryStatus.VALUE_PRESENT));
 
     @Override
     protected List<Pair<MemoryModuleType<?>, MemoryStatus>> getMemoryRequirements() {
@@ -36,36 +38,58 @@ public class EggmorpthTargetTask<E extends AlienEntity> extends ExtendedBehaviou
     }
 
     @Override
-    protected boolean canStillUse(ServerLevel level, E entity, long gameTime) {
+    protected boolean canStillUse(@NotNull ServerLevel level, E entity, long gameTime) {
         return !entity.isFleeing();
     }
 
     @Override
-    protected void tick(ServerLevel level, E entity, long gameTime) {
-        if (entity.isCrawling() ||  entity.isTunnelCrawling())
-            return;
-        var lightSourceLocation = entity.getBrain().getMemory(GigMemoryTypes.NEARBY_NEST_BLOCKS.get()).orElse(null);
-        var target = entity.getFirstPassenger();
-        if (lightSourceLocation == null || !lightSourceLocation.stream().findAny().isPresent())
-            return;
+    protected void tick(@NotNull ServerLevel level, E entity, long gameTime) {
+        if (entity.isCrawling() || entity.isTunnelCrawling() || entity.getFirstPassenger() != null) return;
+        var nestBlockLocation = entity.getBrain().getMemory(GigMemoryTypes.NEARBY_NEST_BLOCKS.get()).orElse(null);
         var test = RandomUtil.getRandomPositionWithinRange(entity.blockPosition(), 3, 1, 3, false, level);
-        var nestLocation = lightSourceLocation.stream().findAny().get().getFirst();
+        var passenger = entity.getFirstPassenger();
+        if (nestBlockLocation != null && nestBlockLocation.stream().findFirst().isPresent()) {
+            var blockPos = nestBlockLocation.stream().findFirst().get().getFirst();
 
-        if (target != null && test != nestLocation)
-            if (!nestLocation.closerToCenterThan(entity.position(), 1.4)) {
-                BrainUtils.setMemory(entity, MemoryModuleType.WALK_TARGET, new WalkTarget(nestLocation, 2.5F, 1));
-            } else {
+            // Check if the block is within the entity's view direction and reachable via pathfinding
+            if (isBlockInViewAndReachable(entity, blockPos)) {
                 for (BlockPos testPos : BlockPos.betweenClosed(test, test.above(2)))
-                    if (level.getBlockState(test).isAir() && level.getBlockState(test.below()).isSolid() && level.getEntitiesOfClass(LivingEntity.class, new AABB(test)).stream().noneMatch(Objects::isNull)) {
+                    if (level.getBlockState(test).isAir() && level.getBlockState(
+                            test.below()).isSolid() && level.getEntitiesOfClass(LivingEntity.class,
+                            new AABB(test)).stream().noneMatch(Objects::isNull)) {
                         BrainUtils.clearMemory(entity, MemoryModuleType.WALK_TARGET);
-                        ((Eggmorphable) target).setTicksUntilEggmorphed(Gigeresque.config.getEggmorphTickTimer());
-                        target.setPos(Vec3.atBottomCenterOf(testPos));
-                        target.removeVehicle();
+                        ((Eggmorphable) passenger).setTicksUntilEggmorphed(Gigeresque.config.getEggmorphTickTimer());
+                        passenger.setPos(Vec3.atBottomCenterOf(testPos));
+                        passenger.removeVehicle();
                         entity.getBrain().eraseMemory(MemoryModuleType.ATTACK_TARGET);
                         level.setBlockAndUpdate(testPos, GigBlocks.NEST_RESIN_WEB_CROSS.defaultBlockState());
                         level.setBlockAndUpdate(testPos.above(), GigBlocks.NEST_RESIN_WEB_CROSS.defaultBlockState());
                     }
+            } else {
+                this.startMovingToTarget(entity, blockPos);
             }
+        }
+    }
+
+    private boolean isBlockInViewAndReachable(E entity, BlockPos blockPos) {
+        var blockCenter = Vec3.atCenterOf(blockPos);
+        var entityPos = entity.position();
+        // Calculate the squared distance between the entity and the block
+        var distanceSquared = blockCenter.distanceToSqr(entityPos);
+
+        // Check if the distance is less than or equal to one block
+        if (distanceSquared <= 1.0) {
+            // Don't start moving towards the target if already within one block distance
+            return false;
+        }
+
+        // Check if the block is reachable via pathfinding
+        var path = entity.getNavigation().createPath(blockPos, 0);
+        return path != null && !path.isDone();
+    }
+
+    private void startMovingToTarget(E alien, BlockPos targetPos) {
+        BrainUtils.setMemory(alien, MemoryModuleType.WALK_TARGET, new WalkTarget(targetPos, 2.5F, 0));
     }
 
 }
