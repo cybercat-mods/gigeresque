65baca020e312610b29d8947ff1a47d9bff7d754
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-16 08:28:28 -0400

    Acid effect will only damage entities not in the resistant tag.

--- src/main/java/mods/cybercat/gigeresque/common/status/effect/impl/AcidStatusEffect.java
+++ src/main/java/mods/cybercat/gigeresque/common/status/effect/impl/AcidStatusEffect.java
@@ -3,6 +3,7 @@ package mods.cybercat.gigeresque.common.status.effect.impl;
 import mod.azure.azurelib.core.object.Color;
 import mods.cybercat.gigeresque.common.Gigeresque;
 import mods.cybercat.gigeresque.common.source.GigDamageSources;
+import mods.cybercat.gigeresque.common.tags.GigTags;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
@@ -21,6 +22,8 @@ public class AcidStatusEffect extends MobEffect {
     @Override
     public void applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
         super.applyEffectTick(entity, amplifier);
-        entity.hurt(GigDamageSources.of(entity.level(), GigDamageSources.ACID), Gigeresque.config.acidDamage * amplifier);
+        if (!entity.getType().is(GigTags.ACID_RESISTANT_ENTITY))
+            entity.hurt(GigDamageSources.of(entity.level(), GigDamageSources.ACID),
+                    Gigeresque.config.acidDamage * amplifier);
     }
 }
