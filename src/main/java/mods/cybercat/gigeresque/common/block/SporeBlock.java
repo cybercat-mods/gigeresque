a3271fe986b68b0c2337e97a2944267875541122
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-16 12:17:09 -0400

    Spore block rotates now

--- src/main/java/mods/cybercat/gigeresque/common/block/SporeBlock.java
+++ src/main/java/mods/cybercat/gigeresque/common/block/SporeBlock.java
@@ -3,25 +3,33 @@ package mods.cybercat.gigeresque.common.block;
 import mods.cybercat.gigeresque.common.block.entity.SporeBlockEntity;
 import mods.cybercat.gigeresque.common.entity.Entities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class SporeBlock extends BaseEntityBlock {
+public class SporeBlock extends BaseEntityBlock implements EntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     protected SporeBlock() {
         super(BlockBehaviour.Properties.of().sound(SoundType.NYLIUM).noOcclusion().strength(15, 15));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
@@ -35,13 +43,20 @@ public class SporeBlock extends BaseEntityBlock {
     }
 
     @Override
-    public @NotNull RenderShape getRenderShape(@NotNull BlockState blockState) {
-        return RenderShape.MODEL;
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
+        return RenderShape.ENTITYBLOCK_ANIMATED;
     }
 
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
         return Block.box(3, 0, 4, 13, 4, 11);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING,
+                context.getHorizontalDirection().getClockWise().getClockWise());
     }
 
 }
