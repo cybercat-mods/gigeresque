14a438eaeaf2f0f5056af8f0924a7790cc60a64f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-16 23:55:10 -0400

    Update LivingEntityMixin.java

--- src/main/java/mods/cybercat/gigeresque/mixins/common/entity/LivingEntityMixin.java
+++ src/main/java/mods/cybercat/gigeresque/mixins/common/entity/LivingEntityMixin.java
@@ -40,8 +40,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Objects;
-
 /**
  * @author Boston Vanseghi
  */
@@ -94,8 +92,8 @@ public abstract class LivingEntityMixin extends Entity implements Host, Eggmorph
 
     @Inject(method = {"hurt"}, at = {@At("HEAD")}, cancellable = true)
     public void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> callbackInfo) {
-        if ((Objects.requireNonNull(this.getVehicle()).getType().is(
-                GigTags.GIG_ALIENS)) && (source == damageSources().drown() || source == damageSources().inWall()) && amount < 1)
+        if (this.getVehicle() != null && this.getVehicle().getType().is(
+                GigTags.GIG_ALIENS) && (source == damageSources().drown() || source == damageSources().inWall()) && amount < 1)
             callbackInfo.setReturnValue(false);
         if (amount >= 2 && this.getFirstPassenger() != null && this.getPassengers().stream().anyMatch(
                 FacehuggerEntity.class::isInstance)) {
