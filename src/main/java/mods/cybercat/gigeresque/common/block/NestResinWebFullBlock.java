2a6ccd768f59e3af031ace2217c53147b48a0cef
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-18 21:51:00 -0400

    Use tag/Constant test

--- src/main/java/mods/cybercat/gigeresque/common/block/NestResinWebFullBlock.java
+++ src/main/java/mods/cybercat/gigeresque/common/block/NestResinWebFullBlock.java
@@ -1,6 +1,5 @@
 package mods.cybercat.gigeresque.common.block;
 
-import mods.cybercat.gigeresque.common.entity.AlienEntity;
 import mods.cybercat.gigeresque.common.util.GigEntityUtils;
 import mods.cybercat.gigeresque.interfacing.Host;
 import net.minecraft.core.BlockPos;
@@ -8,7 +7,6 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,9 +20,9 @@ public class NestResinWebFullBlock extends Block {
 
     @Override
     public void entityInside(@NotNull BlockState state, @NotNull Level world, @NotNull BlockPos pos, @NotNull Entity entity) {
-        if (entity instanceof AlienEntity) return;
-        if (entity instanceof Player playerEntity && (playerEntity.isCreative() || playerEntity.isSpectator())) return;
         if (entity instanceof LivingEntity livingEntity && GigEntityUtils.isTargetHostable(entity) && !((Host) entity).hasParasite()) {
+        if (entity.getType().is(GigTags.GIG_ALIENS)) return;
+        if (Constants.isCreativeSpecPlayer.test(entity)) return;
             livingEntity.makeStuckInBlock(state, new Vec3(0.25, 0.0, 0.25));
             livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 60, 10), entity);
             if (!world.getBlockState(pos.below()).is(GigBlocks.NEST_RESIN_WEB_CROSS))
