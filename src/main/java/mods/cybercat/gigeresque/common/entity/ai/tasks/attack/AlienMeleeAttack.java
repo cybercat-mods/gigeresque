72f35b7a5ca51c5390080264aeed9a41306ebf33
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-20 14:39:36 -0400

    jump at target if in water and target is not.

--- src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/attack/AlienMeleeAttack.java
+++ src/main/java/mods/cybercat/gigeresque/common/entity/ai/tasks/attack/AlienMeleeAttack.java
@@ -8,6 +8,7 @@ import mods.cybercat.gigeresque.common.entity.ai.tasks.CustomDelayedMeleeBehavio
 import mods.cybercat.gigeresque.common.entity.impl.mutant.StalkerEntity;
 import mods.cybercat.gigeresque.interfacing.AbstractAlien;
 import mods.cybercat.gigeresque.interfacing.AnimationSelector;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -17,6 +18,7 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
+import net.minecraft.world.phys.Vec3;
 import net.tslat.smartbrainlib.util.BrainUtils;
 import org.jetbrains.annotations.Nullable;
 
@@ -82,6 +84,14 @@ public class AlienMeleeAttack<E extends PathfinderMob & AbstractAlien & GeoEntit
         if (entity instanceof StalkerEntity) {
             this.target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200, 1, true, true));
             this.target.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 200, 1, true, true));
+        }
+
+        if (entity.isInWater() && !target.isInWater()) {
+            Vec3 vec3 = entity.getDeltaMovement();
+            Vec3 vec32 = new Vec3(this.target.getX() - entity.getX(), 0.0, this.target.getZ() - entity.getZ());
+            if (vec32.lengthSqr() > 1.0E-7)
+                vec32 = vec32.normalize().scale(1.4).add(vec3.scale(0.2));
+            entity.setDeltaMovement(vec32.x, 1.3F, vec32.z);
         }
 
         entity.swing(InteractionHand.MAIN_HAND);
