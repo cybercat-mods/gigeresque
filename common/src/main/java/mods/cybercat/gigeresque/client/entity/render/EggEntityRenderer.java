bf36a05ee08d83c9543e765b2be35aa215b59cac
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-06 22:39:46 -0400

    Remove layer due to alpha issues.

--- common/src/main/java/mods/cybercat/gigeresque/client/entity/render/EggEntityRenderer.java
+++ common/src/main/java/mods/cybercat/gigeresque/client/entity/render/EggEntityRenderer.java
@@ -2,14 +2,12 @@ package mods.cybercat.gigeresque.client.entity.render;
 
 import mod.azure.azurelib.common.api.client.renderer.GeoEntityRenderer;
 import mods.cybercat.gigeresque.client.entity.model.EggEntityModel;
-import mods.cybercat.gigeresque.client.entity.render.feature.EggDyingFeatureRenderer;
 import mods.cybercat.gigeresque.common.entity.impl.classic.AlienEggEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 
 public class EggEntityRenderer extends GeoEntityRenderer<AlienEggEntity> {
     public EggEntityRenderer(EntityRendererProvider.Context context) {
         super(context, new EggEntityModel());
-        this.addRenderLayer(new EggDyingFeatureRenderer(this));
         this.shadowRadius = 0.5f;
     }
 
