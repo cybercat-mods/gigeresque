87591f813c46f62609d3f307684270435ff474c0
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-02-06 21:40:27 -0500

    Update ChestbursterEntity.java

--- common/src/main/java/mods/cybercat/gigeresque/common/entity/impl/classic/ChestbursterEntity.java
+++ common/src/main/java/mods/cybercat/gigeresque/common/entity/impl/classic/ChestbursterEntity.java
@@ -26,7 +26,6 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
