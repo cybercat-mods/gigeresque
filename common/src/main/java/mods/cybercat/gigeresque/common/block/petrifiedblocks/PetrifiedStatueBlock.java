683f6c0749a8e8d9de6aaf51efa403d3409c3a05
Zesheir <nicholasradtke@me.com>
2025-09-10 22:18:40 -0500

    Added Unnerving Statue

--- common/src/main/java/mods/cybercat/gigeresque/common/block/petrifiedblocks/PetrifiedStatueBlock.java
+++ common/src/main/java/mods/cybercat/gigeresque/common/block/petrifiedblocks/PetrifiedStatueBlock.java
@@ -3,16 +3,10 @@ package mods.cybercat.gigeresque.common.block.petrifiedblocks;
 import com.mojang.serialization.MapCodec;
 import mods.cybercat.gigeresque.common.block.GigBlocks;
 import mods.cybercat.gigeresque.common.block.petrifiedblocks.entity.PetrifiedStatueEntity;
-import mods.cybercat.gigeresque.common.block.storage.StorageStates;
 import mods.cybercat.gigeresque.common.entity.GigEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -30,7 +24,6 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class PetrifiedStatueBlock extends BaseEntityBlock {
 
