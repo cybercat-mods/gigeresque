39a3c042a4355ba40e84291df2ad27408f8460ed
Zesheir <nicholasradtke@me.com>
2025-09-17 23:40:22 -0500

    Non neo hosts are now immune to mycosis

--- common/src/main/java/mods/cybercat/gigeresque/common/status/effect/impl/SporeStatusEffect.java
+++ common/src/main/java/mods/cybercat/gigeresque/common/status/effect/impl/SporeStatusEffect.java
@@ -1,6 +1,8 @@
 package mods.cybercat.gigeresque.common.status.effect.impl;
 
 import mod.azure.azurelib.core.object.Color;
+import mods.cybercat.gigeresque.common.tags.GigTags;
+import mods.cybercat.gigeresque.common.util.GigEntityUtils;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
@@ -19,6 +21,7 @@ import mods.cybercat.gigeresque.common.entity.GigEntities;
 import mods.cybercat.gigeresque.common.source.GigDamageSources;
 import mods.cybercat.gigeresque.common.status.effect.GigStatusEffects;
 import mods.cybercat.gigeresque.common.util.DamageSourceUtils;
+import org.jetbrains.annotations.NotNull;
 
 public class SporeStatusEffect extends MobEffect {
 
@@ -38,6 +41,9 @@ public class SporeStatusEffect extends MobEffect {
             return;
         if (entity instanceof Mob mob && mob.isNoAi())
             return;
+        if (!entity.getType().is(GigTags.NEOHOST)){
+            return;
+        }
         var burster = GigEntities.NEOBURSTER.get().create(entity.level());
         if (burster != null) {
             setBursterProperties(entity, burster);
