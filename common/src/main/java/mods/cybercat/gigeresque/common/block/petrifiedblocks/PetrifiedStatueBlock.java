ab4c9b9eeda46b85b8c41939e7bf184727040d63
Zesheir <nicholasradtke@me.com>
2025-09-30 16:39:47 -0500

    add unfinished tooltip to unnerving statue

--- common/src/main/java/mods/cybercat/gigeresque/common/block/petrifiedblocks/PetrifiedStatueBlock.java
+++ common/src/main/java/mods/cybercat/gigeresque/common/block/petrifiedblocks/PetrifiedStatueBlock.java
@@ -1,11 +1,15 @@
 package mods.cybercat.gigeresque.common.block.petrifiedblocks;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -22,6 +26,8 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 import mods.cybercat.gigeresque.common.block.GigBlocks;
 import mods.cybercat.gigeresque.common.block.petrifiedblocks.entity.PetrifiedStatueEntity;
@@ -119,6 +125,21 @@ public class PetrifiedStatueBlock extends BaseEntityBlock {
             if (!testPos.equals(pos) && !world.getBlockState(testPos).isAir())
                 return false;
         return true;
+    }
+
+    @Override
+    public void appendHoverText(
+        @NotNull ItemStack stack,
+        Item.@NotNull TooltipContext context,
+        @NotNull List<Component> tooltipComponents,
+        @NotNull TooltipFlag tooltipFlag
+    ) {
+        tooltipComponents.add(
+            Component.translatable("block.gigeresque.unfinished.tooltip")
+                .withStyle(ChatFormatting.DARK_RED)
+                .withStyle(ChatFormatting.ITALIC)
+        );
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
     @Override
