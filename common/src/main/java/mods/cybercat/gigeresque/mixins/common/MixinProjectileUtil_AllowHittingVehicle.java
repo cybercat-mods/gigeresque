69ab3a97f748f850c09e1c3d61d446fe9be76261
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-01 18:25:20 -0400

    Fixes Mixin conflict with AVP and possible other mods
    Should be used in favour of Redirects when you are wrapping the original operation and not replacing it outright, as unlike Redirects, this chains when used by multiple people.

--- common/src/main/java/mods/cybercat/gigeresque/mixins/common/MixinProjectileUtil_AllowHittingVehicle.java
+++ common/src/main/java/mods/cybercat/gigeresque/mixins/common/MixinProjectileUtil_AllowHittingVehicle.java
@@ -1,31 +1,32 @@
 package mods.cybercat.gigeresque.mixins.common;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import mods.cybercat.gigeresque.common.tags.GigTags;
 
 @Mixin(ProjectileUtil.class)
 public class MixinProjectileUtil_AllowHittingVehicle {
 
-    @Redirect(
+    @WrapOperation(
         method = "getEntityHitResult(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;D)Lnet/minecraft/world/phys/EntityHitResult;",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/Entity;getRootVehicle()Lnet/minecraft/world/entity/Entity;"
         )
     )
-    private static Entity modifyRootVehicleCheck(Entity entity, Entity shooter) {
+    private static Entity modifyRootVehicleCheck(Entity instance, Operation<Entity> original) {
         // Check if an entity is alien.
-        if (gig$isAlien(entity)) {
+        if (gig$isAlien(instance)) {
             return null;
         }
 
-        return entity.getRootVehicle();
+        return instance.getRootVehicle();
     }
 
     @Unique
