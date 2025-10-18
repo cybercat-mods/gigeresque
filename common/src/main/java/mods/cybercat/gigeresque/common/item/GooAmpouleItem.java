39c0b2fa6f3116d94f0eca44b8e1d140811d1eaa
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-10-18 00:52:55 -0400

    Adds not finished tooltips for Ampoule items

--- common/src/main/java/mods/cybercat/gigeresque/common/item/GooAmpouleItem.java
+++ common/src/main/java/mods/cybercat/gigeresque/common/item/GooAmpouleItem.java
@@ -1,16 +1,23 @@
 package mods.cybercat.gigeresque.common.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Position;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ArrowItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import mods.cybercat.gigeresque.common.entity.impl.projectile.GooAmpouleProjectile;
+
+import java.util.List;
 
 public class GooAmpouleItem extends ArrowItem {
 
@@ -18,13 +25,28 @@ public class GooAmpouleItem extends ArrowItem {
         super(properties);
     }
 
+	@Override
+	public void appendHoverText(
+		@NotNull ItemStack stack,
+		Item.@NotNull TooltipContext context,
+		@NotNull List<Component> tooltipComponents,
+		@NotNull TooltipFlag tooltipFlag
+	) {
+		tooltipComponents.add(
+			Component.translatable("block.gigeresque.unfinished.tooltip")
+				.withStyle(ChatFormatting.DARK_RED)
+				.withStyle(ChatFormatting.ITALIC)
+		);
+		super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+	}
+
     @Override
-    public AbstractArrow createArrow(Level level, ItemStack ammo, LivingEntity shooter, @Nullable ItemStack weapon) {
+    public @NotNull AbstractArrow createArrow(@NotNull Level level, ItemStack ammo, @NotNull LivingEntity shooter, @Nullable ItemStack weapon) {
         return new GooAmpouleProjectile(level, shooter, ammo.copyWithCount(1), weapon);
     }
 
     @Override
-    public Projectile asProjectile(Level level, Position pos, ItemStack stack, Direction direction) {
+    public @NotNull Projectile asProjectile(@NotNull Level level, @NotNull Position pos, @NotNull ItemStack stack, @NotNull Direction direction) {
         return null;
     }
 }
